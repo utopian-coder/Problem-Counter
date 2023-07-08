a1e899e29ac0b9852ab93215cda3e37d51151761
Imran Biswas <imranbiswas2209@gmail.com>
2023-07-08 13:02:30 +0530

    Update main container padding.

--- src/components/LayoutComponents/Layout.jsx
+++ src/components/LayoutComponents/Layout.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 
 const Layout = () => {
   return (
-    <main className='w-full h-screen px-20'>
+    <main className='w-full h-screen px-2 md:px-20'>
       <Header />
       <Outlet />
     </main>
